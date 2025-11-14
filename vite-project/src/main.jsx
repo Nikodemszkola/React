acd24f46a61f64e7961921caebe1fc06a0f1d7ff
niko <niko@gmail.com>
2025-11-14 12:04:15 +0100

    awdaw

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<>
+    <h1>Kursy Online</h1>
+   <div class="przegladaj">Przeglądaj</div>
+   <div id="menu"></div>
+</>
 )
+
