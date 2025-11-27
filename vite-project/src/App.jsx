cfa0cc2a239a2da3db99be78ebfe0b99c8d1c366
KrystianT <krystian.tomczyk@tm1.edu.pl>
2025-11-28 00:07:58 +0100

    more courses

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -34,8 +34,56 @@ function App() {
       price: 29.99,
       category: 'Data Science'
     },
-    {
+        {
       id: 4,
+      title: 'Java Spring Framework & Spring Boot',
+      description: 'Master Java, Spring, Spring Boot, Docker and Microservices. Ideal for beginners and intermediate developers.',
+      image: 'https://img-c.udemycdn.com/course/240x135/2167814_a0e6_5.jpg',
+      price: 39.99,
+      category: 'Backend'
+    },
+    {
+      id: 5,
+      title: 'React od Podstaw do Eksperta',
+      description: 'Kompletny przewodnik po React, Hooks, Redux oraz Next.js. Zbuduj 5 realnych projektów.',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+      price: 49.99,
+      category: 'Frontend'
+    },
+    {
+      id: 6,
+      title: 'Python dla Analizy Danych',
+      description: 'Naucz się bibliotek Pandas, NumPy, Matplotlib i wejdź w świat Data Science.',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+      price: 29.99,
+      category: 'Data Science'
+    },
+        {
+      id: 7,
+      title: 'Java Spring Framework & Spring Boot',
+      description: 'Master Java, Spring, Spring Boot, Docker and Microservices. Ideal for beginners and intermediate developers.',
+      image: 'https://img-c.udemycdn.com/course/240x135/2167814_a0e6_5.jpg',
+      price: 39.99,
+      category: 'Backend'
+    },
+    {
+      id: 8,
+      title: 'React od Podstaw do Eksperta',
+      description: 'Kompletny przewodnik po React, Hooks, Redux oraz Next.js. Zbuduj 5 realnych projektów.',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+      price: 49.99,
+      category: 'Frontend'
+    },
+    {
+      id: 9,
+      title: 'Python dla Analizy Danych',
+      description: 'Naucz się bibliotek Pandas, NumPy, Matplotlib i wejdź w świat Data Science.',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+      price: 29.99,
+      category: 'Data Science'
+    },
+    {
+      id: 10,
       title: 'UI/UX Design Masterclass',
       description: 'Projektowanie interfejsów w Figma. Teoria kolorów, typografia i prototypowanie.',
       image: 'https://s3-alpha.figma.com/hub/file/1481185752/fc90e7a2-f87c-41c3-9d41-3b7630737a23-cover.png',
