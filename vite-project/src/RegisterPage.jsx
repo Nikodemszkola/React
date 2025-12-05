e971b779358eec8afaf5c193d1dfa9370fe3132c
niko <niko@gmail.com>
2025-12-05 10:49:51 +0100

    dwad

--- vite-project/src/RegisterPage.jsx
+++ vite-project/src/RegisterPage.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import './Auth.css';
 
 function RegisterPage({ onSwitchToLogin }) {
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert('Rejestracja w trakcie implementacji...');
+  
+    const nickname = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    try {
+      const res = await fetch("http://localhost/kursy/register.php", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nickname, email, password })
+      });
+  
+      
+      const text = await res.text();
+      let data;
+      try {
+        data = JSON.parse(text);
+      } catch (err) {
+        alert("Serwer nie zwrócił poprawnego JSON!");
+        console.error(text);
+        return;
+      }
+  
+      if (data.status === "ok") {
+        alert(data.message);
+      } else {
+        alert("Błąd: " + data.message);
+      }
+  
+    } catch (error) {
+      alert("Błąd połączenia z serwerem!");
+      console.error(error);
+    }
   };
+  
 
   return (
     <div className="auth-container">
